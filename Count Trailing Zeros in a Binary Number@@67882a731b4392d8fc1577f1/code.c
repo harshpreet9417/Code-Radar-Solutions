07a8d1ec765e6e74cbaebd3804369f06188e2a41
harshpreet9417 <kharshpreet530@gmail.com>
2025-02-16 12:02:53 +0530

    Solution to Count Trailing Zeros in a Binary Number

--- Count Trailing Zeros in a Binary Number@@67882a731b4392d8fc1577f1/code.c
+++ Count Trailing Zeros in a Binary Number@@67882a731b4392d8fc1577f1/code.c
@@ -1,8 +1,20 @@
 // Your code here...
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    printf("%b",n);
+#include <stdio.h>
+int countTrailingZeroes(int n) {
+    int count = 0;
+    while ((n & 1) == 0 && n != 0) {
+        count++;
+        n >>= 1;
+    }
+    return count;
+}
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    int result = countTrailingZeroes(num);
+    printf(" %d\n", result);
+
     return 0;
 }
